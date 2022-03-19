66dee8616b1f848ac6dc9cb839b9fd72cc2a6646
Dadi Nur Ardhi <dadinurardhi@gmail.com>
2022-03-20 00:47:41 +0700

    fix bug

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $(document).ready(() => {
   let currentNumber = '0';
 
   const updateScreen = (number) => {
+    console.log(number);
     $('.calculator-screen').attr({ value: number });
   };
 
@@ -65,7 +66,6 @@ $(document).ready(() => {
 
   $('.operator').click((e) => {
     inputOperator(e.target.value);
-    updateScreen(currentNumber);
   });
 
   $('.percentage').click((e) => {
@@ -87,3 +87,119 @@ $(document).ready(() => {
     updateScreen(currentNumber);
   });
 });
+
+/*
+// const numbers = document.querySelectorAll(".number")
+
+// numbers.forEach ((number) => {
+//     number.addEventListener("click", (event) => {
+//         console.log(event.target.value)
+//     })
+// })
+
+// const calculatorScreen = document.querySelector('.calculator-screen')
+
+// const updateScreen = (number) => {
+//     calculatorScreen.value = number
+// }
+
+// const number = document.querySelectorAll(".number")
+
+// number.forEach((number) => {
+//     number.addEventListener("click", (event) => {
+//         updateScreen(event.target.value)
+//     })
+// })
+
+// let prevNumber = ''
+// let calculationOperator = ''
+// let currentNumber = '0'
+
+// const inputNumber = (number) => {
+//     if (currentNumber === '0') {
+//         currentNumber = number
+//     } else {
+//         currentNumber += number
+//     }
+// }
+
+// numbers.forEach((number) => {
+//     number.addEventListener("click", (event) => {
+//         inputNumber(event.target.value)
+//         updateScreen(currentNumber)
+//     })
+// })
+
+// const operators = document.querySelectorAll(".operator")
+
+// operators.forEach((operator) => {
+//     operator.addEventListener("click", (event) => {
+//         inputOperator(event.target.value)
+//     })
+// })
+
+// const inputOperator = (operator) => {
+//     if (calculationOperator === '') {
+//         prevNumber = currentNumber
+//     }
+//     calculationOperator = operator
+//     currentNumber = ''
+// }
+
+// const equalSign = document.querySelector('.equal-sign')
+
+// equalSign.addEventListener('click', () => {
+//     calculate()
+//     updateScreen(currentNumber)
+// })
+
+// const calculate = () => {
+//     let result = ''
+//     switch(calculationOperator) {
+//         case "+":
+//             result = parseFloat (prevNumber) + parseFloat (currentNumber)
+//             break
+//         case "-":
+//             result = parseFloat (prevNumber) - parseFloat (currentNumber)
+//             break
+//         case "*":
+//             result = parseFloat (prevNumber) * parseFloat (currentNumber)
+//             break
+//         case "/":
+//             result = parseFloat (prevNumber) / parseFloat (currentNumber)
+//             break
+//         default:
+//             break
+//     }
+//     currentNumber = result
+//     calculationOperator = ''
+// }
+
+// const clearAll = () => {
+//     prevNumber = ''
+//     calculationOperator = ''
+//     currentNumber = '0'
+// }
+
+// const clearBtn = document.querySelector('.all-clear')
+
+// clearBtn.addEventListener ('click', () => {
+//     clearAll()
+//     updateScreen(currentNumber)
+// })
+
+const decimal = document.querySelector('.decimal')
+
+inputDecimal = (dot) => {
+    if (currentNumber.includes('.')) {
+        return
+    }
+    currentNumber += dot
+}
+
+decimal.addEventListener('click', (event) => {
+    inputDecimal(event.target.value)
+    updateScreen(currentNumber)
+})
+
+*/
